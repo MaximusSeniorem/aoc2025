568d59e2c46bcbf6d9ae47237d177ce4533a8189
MaximusSeniorem <9930974+MaximusSeniorem@users.noreply.github.com>
2025-12-04 23:43:20 +0100

    day02 - part 1 done!

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,86 +1,116 @@
-use std::ops::{ RangeInclusive };
-use std::num::ParseIntError;
+use::std::fmt;
+use std::ops::RangeInclusive;
 use std::str::FromStr;
+use std::num::ParseIntError;
+
 
 #[derive(Debug)]
-enum Err {
-  ParseIntError(ParseIntError),
-  ParseError,
+pub enum Err<E> {
+    MissingSeparator,
+    Start(E),
+    End(E),
 }
 
-impl From<ParseIntError> for Err {
-  fn from(e: ParseIntError) -> Self {
-    Err::ParseIntError(e)
-  }
+impl<E: fmt::Display> fmt::Display for Err<E> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Err::MissingSeparator =>
+                write!(f, "missing '-' separator in range"),
+            Err::Start(e) =>
+                write!(f, "error parsing start of range: {e}"),
+            Err::End(e) =>
+                write!(f, "error parsing end of range: {e}"),
+        }
+    }
 }
+
+impl<E: std::error::Error + 'static> std::error::Error for Err<E> {}
+
 
 #[derive(Debug)]
-struct RangeIds {
-  first: String,
-  last: String,
-  range : RangeInclusive<u32>,
-}
+struct RangeIds<T>(pub RangeInclusive<T>);
 
-impl RangeIds {
-  fn new (s: &str) -> Result<Self, Err> {
-    s.parse()
-  } 
-}
-
-impl FromStr for RangeIds {
-      type Err = crate::Err;  
+impl<T> FromStr for RangeIds<T> where T: FromStr{
+  type Err = crate::Err<T::Err>;  
   
   fn from_str(s: &str) -> Result<Self, Self::Err> {  
-    match s.split_once('-'){
-      Some((f, l)) => Ok(
-        Self{
-          first: f.to_string(), 
-          last: l.to_string(),
-          range: RangeInclusive::new(f.parse::<u32>()?, l.parse::<u32>()?)
-        }),
-      None => Err(Err::ParseError),
-    } 
+    let (f, l) = s.split_once('-').ok_or(Err::MissingSeparator)?;
+    let start: T = f.parse().map_err(Err::Start)?;
+    let end: T = l.parse().map_err(Err::End)?;
+    Ok(RangeIds(start..=end))
   }
 }
 
-fn handle_input(input : &str, inv_ids : &mut Vec<u32>) -> Result<(), Err> {
-  let r = RangeIds::new(input)?;
-  //println!("{:?}, contains? : {}, {}", r, r.range.contains(&r.range.start()), r.range.contains(&r.range.end()));
 
-  let len_diff = r.last.len() - r.first.len();
-  if len_diff == 0 && (r.first.len() % 2) != 0 { 
-    println!("[{:?}] does not contains bad ids", r.range);
+fn handle_input(input : &str, acc: &mut u64) -> Result<(), Err<ParseIntError>> {
+  let RangeIds(r): RangeIds<u64> = input.parse()?;
+
+  //setup
+  let start_log = r.start().ilog(10);
+  let end_log = r.end().ilog(10);
+  
+  let is_log_pair = start_log % 2 == 0;
+  let is_log_diff = end_log - start_log != 0;
+
+  if !is_log_diff && is_log_pair { 
+    println!("{} contains no invalid ids", input);
     return Ok(()); 
   }
   else {
-    print!("[{:?}], ", r.range);
-
+    print!("{}", input);
   }
 
-  let isStartPair = r.first.len() % 2 == 0;
-  let mut n = if isStartPair { r.first.len() } else { r.first.len() + 1 } / 2; 
-  let mut cur10pow = u32::pow(10, n as u32);
-  let start = if isStartPair { r.first[0..n].parse()? } else { cur10pow };
-  let mut start = start * cur10pow + start;
+  let n = if is_log_pair { (start_log + 2) / 2 } 
+      else { (start_log + 1) / 2 };
+  let mut cur_pow = u64::pow(10, n);
+  let mut pattern = if is_log_pair { cur_pow / 10 } else { r.start() / cur_pow };
+  let mut cur_bad_id = pattern * cur_pow + pattern;
 
-  while !r.range.contains(&start) && start <= *r.range.end(){
-    start += cur10pow + 1;
+  while !r.contains(&cur_bad_id) && cur_bad_id < *r.end() {
+    pattern += 1;
+    if pattern == cur_pow { cur_pow *= 10; }
+
+    cur_bad_id = pattern * cur_pow + pattern;
   }
-
-  println!("start cond = {}, {}", n, start);
+  if !r.contains(&&cur_bad_id)  { 
+    println!(" contains no invalid ids");
+    return Ok(()); 
+  }
   
+  //ok lets go
+  let mut bad_ids: Vec<u64> = Vec::new();
+
+  while r.contains(&cur_bad_id) {
+    bad_ids.push(cur_bad_id);
+    
+    pattern += 1;
+    if pattern == cur_pow { cur_pow *= 10; }
+
+    cur_bad_id = pattern * cur_pow + pattern;
+  }
+  if bad_ids.is_empty() {
+    println!(" contains no invalid ids");
+  }
+  else{
+    println!(" contains {:?}", bad_ids);
+  }
+
+  *acc += bad_ids.into_iter().sum::<u64>();
+
   Ok(())
 }
 
 fn main() {
-  let mut inv_ids = Vec::<u32>::new();
-  for input in TEST_INPUT.split(','){
-    handle_input(input, &mut inv_ids).expect("parsing error");
+  let mut acc: u64 = 0;
+  for input in REAL_INPUT.split(','){
+    handle_input(input, &mut acc).expect("parsing error");
   }
+
+  println!("sum of bad ids : {}", acc);
 }
 
 
-const TEST_INPUT : &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+const TEST_INPUT : &str = "1-9,456-999,11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
 
 const REAL_INPUT : &str = "1090286-1131879,3259566-3404881,138124-175118,266204727-266361099,16765-24272,7657360692-7657593676,88857504-88926597,6869078-6903096,48444999-48532270,61427792-61580535,71-103,8077-10421,1920-2560,2-17,951-1259,34-50,28994-36978,1309-1822,9393918461-9393960770,89479-120899,834641-988077,5389718924-5389797353,34010076-34214499,5063-7100,607034-753348,19098586-19261191,125085556-125188689,39839-51927,3246-5037,174-260,439715-473176,187287-262190,348-535,58956-78301,4388160-4505757,512092-584994,13388753-13534387";
